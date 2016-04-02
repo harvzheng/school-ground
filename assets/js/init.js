9aaeedc0d879f571ab903dcf7cac286acef65420
Harvey Zheng <zheng.harvey5@gmail.com>
2016-04-02 09:56:00 -0400

    Initial Commit

---
+++ assets/js/init.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".dropdown-button").dropdown();
+    $('.scrollspy').scrollSpy();
+    $(".button-collapse").sideNav();
+    $('.slider').slider({
+        full_width: true
+        , interval: 5000
+        , height: 700
+    });
+});
